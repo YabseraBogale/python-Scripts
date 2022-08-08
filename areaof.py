7a638585c2f17f3fb500428fe53591d623e2a1e9
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-08-08 14:51:01 +0000

    Add files via upload

---
+++ areaof.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+r=int(input("Enter the radius: "))
+area=(pow(r,2))*2*math.pi
+print("Area of the circle is ",area)
+o=open('hello.py')
+for line in o:
+       print(line)
+
+    
