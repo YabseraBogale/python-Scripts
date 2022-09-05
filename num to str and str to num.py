0a1ecaaaa83e7a1fb67ed6ac4877f2c309418fcb
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-09-05 18:08:33 +0300

    Add files via upload

---
+++ num to str and str to num.py
@@ -0,0 +1,14 @@
+
+""""
+day sep 5 2022
+""""
+def int_to_str(num):
+	num=str(num)
+	return num
+def str_to_int(num):
+	num=int(num)
+	return num
+num1=int_to_str(4)
+num2=str_to_int(4)
+print("int to str",num1)
+print("str to int",num2)
