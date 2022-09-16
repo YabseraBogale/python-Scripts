21e92e2c7d9d068c438f877ea91a6802e930d2c3
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-09-16 20:28:31 +0300

    Add files via upload

---
+++ allst.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 10:16:32 2022
+ACESS HASH=5791991286168741736
+chat id= 1562041979
+@author: yabsera
+"""
+import pandas as pd
+
+df=pd.read_excel('member2.xlsx')
+print(df.descibe())
+print(df.shape)
