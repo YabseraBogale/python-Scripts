cdff87380eda9a71e1459edc8860c45b3e4f3120
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2022-12-19 10:15:37 +0300

    Add files via upload

---
+++ SimpleAnimationTest_v5.py
@@ -0,0 +1,79 @@
+
+from time import sleep
+import random
+import numpy as np
+import os
+
+
+def number():
+	lst_of_lst=[]
+	i=0
+	while i<30:
+		j=0
+		lst=[]
+		while j<30:
+			num=random.randint(0,1)
+			lst.append(num)
+			j+=1
+		lst_of_lst.append(lst)
+		i+=1
+	return lst_of_lst
+
+
+def Game(numbers):
+	game=numbers			
+	i=0
+	while i<30:
+		if i==0:
+			lst1=game[i]
+			lst2=game[i+1]
+			lst3=number()[random.randint(0,30)]
+		elif i==29:
+			break
+		else:
+			lst1=game[i]
+			lst2=game[i+1]
+			lst3=game[i-1]
+		j=0
+		while j<30:
+			if j!=28:
+				up=lst3[j]+lst3[j+1]+lst3[j+2]
+				low=lst2[j]+lst2[j+1]+lst2[j+2]
+				if j!=0:
+					middle=lst1[j+1]+lst1[j-1]
+				else:
+					middle=lst1[j+1]+lst2[random.randint(0,29)]
+			else:
+				break
+			state=up+middle+low
+			if state<2:
+				game[i][j]=1
+			elif state==2:
+				game[i][j]=lst1[j]
+			elif state==3 and lst1[j]==1:
+				game[i][j]=0
+			elif state==3 and lst1[j]==0:
+				game[i][j]=1
+			j+=1
+		i+=1
+	return game
+def Change(numbers):
+	lst=str(np.array(numbers))
+	lst=lst.replace('1','-').replace('[','').replace(']','').replace('0','^').replace('\n','\n\t\t\t\t\t\t')
+	return '\n\n\n\t\t\t\t\t\t '+lst
+ 
+def display():
+	conway=open('GameOflife.txt','w')
+	numbers=number()
+	while True:
+		num=random.randint(0,1)
+		if num==1:
+			numbers=Game(numbers)
+		else:
+			numbers=number()
+		conway.write(Change(numbers))
+		print(Change(numbers),)
+		sleep(2)
+		os.system('clear')
+print(display())
+
