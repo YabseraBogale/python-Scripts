24fa570ef291f0fe5459be8732385b590b6c05b9
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-03-10 18:28:56 +0300

    Add files via upload

---
+++ Nextsmallernumberwiththesamedigits.py
@@ -0,0 +1,18 @@
+"""
+in build haven't passed yet
+
+"""
+
+
+def splitter(n):
+    lst=[]
+    for i in str(n):
+        lst.append(i)
+    return lst
+def next_smaller(n):
+    lst=splitter(n)
+    return lst
+    
+    
+    
+print(next_smaller(907))
