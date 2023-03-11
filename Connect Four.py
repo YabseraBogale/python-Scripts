a605644668889a93d0e3e6809df2498651f83879
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-03-11 19:31:24 +0300

    Add files via upload

---
+++ Connect Four.py
@@ -0,0 +1,26 @@
+import numpy as np
+def moves(lst):
+      #A B C D F G 
+      #1
+      #2
+      #3
+      #4
+      #5
+      #6
+      #7
+    A=[0,0,0,0,0,0,]
+    return arr
+print(moves())
+    
+    
+def who_is_winner(pieces_position_list):
+    # Your code here!
+    lst=[]
+    for i in pieces_position_list:
+        lst.append(i.split("_"))
+    return lst
+print(who_is_winner([ 
+"C_Yellow", "E_Red", "G_Yellow", "B_Red", "D_Yellow", "B_Red", "B_Yellow", "G_Red", "C_Yellow", "C_Red",
+"D_Yellow", "F_Red", "E_Yellow", "A_Red", "A_Yellow", "G_Red", "A_Yellow", "F_Red", "F_Yellow", "D_Red", 
+"B_Yellow", "E_Red", "D_Yellow", "A_Red", "G_Yellow", "D_Red", "D_Yellow", "C_Red"
+]))
