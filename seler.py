d13acecd44fb32e8f725c20a1a45d412b376e656
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-04-12 19:03:16 +0300

    Add files via upload

--- seler.py
+++ seler.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
+
 driver = webdriver.Chrome()
+url2="https://www.selenium.dev/documentation/webdriver/elements/finders/"
 url="https://debank.com/profile/0x0c1a3e4e1c3da4c89582dfa1afa87a1853d7f78f/history?chain=metis"
 driver.get(url)
 
-title = driver.title
-print(title)
 
-driver.implicitly_wait(0.5)
+driver.implicitly_wait(20)
+data=driver.find_elements(By.CLASS_NAME,"History_tableLine__3dtlF")
+date=data[0].find_element(By.CLASS_NAME,"History_sinceTime__3JN2E").text
 
+print(date)
 driver.quit()
+
