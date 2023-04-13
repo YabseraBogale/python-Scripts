2d125cccecae0d4f6af4ac04b5604d72cdf6d9ae
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-04-13 19:53:52 +0300

    Add files via upload

--- seler.py
+++ seler.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def removeStr(str):
+    lst=[]
+    symbole=""
+    for i in str:
+        if i.isdigit() or i=="." or i==",":
+            lst.append(i)
+        else:
+            symbole+=i
 
+    return ["".join((i for i in lst)),symbole]
+def scrape_debank(address, chain):
+    
+    driver = webdriver.Chrome()
+    url=f'https://debank.com/profile/{address}/history?chain={chain}'
+    driver.get(url)
+    driver.implicitly_wait(20)
+    data=driver.find_elements(By.CLASS_NAME,"History_tableLine__3dtlF")
+    date=data[0].find_element(By.CLASS_NAME,"History_sinceTime__3JN2E").text
 
-driver = webdriver.Chrome()
-url2="https://www.selenium.dev/documentation/webdriver/elements/finders/"
-url="https://debank.com/profile/0x0c1a3e4e1c3da4c89582dfa1afa87a1853d7f78f/history?chain=metis"
-driver.get(url)
-
-
-driver.implicitly_wait(20)
-data=driver.find_elements(By.CLASS_NAME,"History_tableLine__3dtlF")
-date=data[0].find_element(By.CLASS_NAME,"History_sinceTime__3JN2E").text
-
-print(date)
-driver.quit()
+    print(date)
+    driver.quit()
 
