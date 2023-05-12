4a0434ba5df0b0659db822a8593a46e8fa6ddb1e
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-05-12 19:57:43 +0300

    Add files via upload

---
+++ primeSum.py
@@ -0,0 +1,22 @@
+
+"""
+    this code is not optimzided enough
+"""
+def primeSum(limit):
+    num=limit+1
+    sum=0
+    i=2
+    while i<num:
+        j=2
+        yes_no=False
+        while j<i:
+            if i%j==0:
+                yes_no=True
+            j+=1
+        if yes_no==False:
+            sum+=i
+        i+=1
+        
+    return sum
+
+print(primeSum(2000000))
