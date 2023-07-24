b6b4c47cf8588ade68cf0b3deb8a41a5ba279123
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-07-24 17:37:20 +0300

    Add files via upload

--- names.py
+++ names.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from requests import get
+from pprint import pprint
 
 import sqlite3
 
@@ -9,15 +10,21 @@ data=get(url).text
 
 soup=bs(data,'html.parser')
 test=soup.find_all('tr')[3].find_all('td')
-print(test[1],test[3])
+
 lst=[]
 for i in soup.find_all('tr'):
     tr=str(i.find_all('td')).replace('<td>','').replace('</td>','')
     if(tr.find('[')!=-1):
         lst.append(tr)
 
-for i in range(2,102):
-    print(lst[i])
+def removesemiComma(lst):
+    lst2=[]
+    for i in lst:
+        lst2.append(i.replace(',',''))
+    return lst2
 
+for i in range(2,102):
+    lst2=list(lst[i].replace('[','').replace(']','').split(' '))
+    print(removesemiComma(lst2[1:]))
 
 
